48ad8eb0ade5920c58c061276247f4bb2b8b864b
Rob McGuire-Dale <rob@osuosl.org>
2009-09-14 18:22:31 -0700

    Final tests written. Bugs found and fixed with resizing with parent larger than object.

--- jquery.scale.js
+++ jquery.scale.js
@@ -4,7 +4,7 @@
  * licensed under GPLv3.
  * Date: 9/4/2009
  *
- * @projectDescription jQuery extension for scaling an object inside its parent while maintaining the aspect ratio
+ * @projectDescription jQuery extension for scaling an object to fit inside its parent while maintaining the aspect ratio
  * @author Rob McGuire-Dale -  rob.mcguiredale@gmail.com
  * @version 1.0
  *
@@ -15,18 +15,13 @@
  *
  */
 
-jQuery.fn.scale = function()
+jQuery.fn.scale = function( center )
 {       
     console.log( "Proportionally scaling stuff..." );
     
-    // grab the object and parent's height and width
-    oHeight = this.outerHeight( true );
-    oWidth = this.outerWidth( true );
-    pHeight = this.parent().innerHeight();
-    pWidth = this.parent().innerWidth();
-    
     // Object too tall, but width is fine. Need to shorten.
-    if( oHeight > pHeight && oWidth < pWidth ){
+    if( this.outerHeight() > this.parent().innerHeight() && 
+        this.outerWidth() < this.parent().innerWidth() ){
         
         console.log( "Object too tall, but width is fine. Need to shorten." );
         
@@ -34,7 +29,8 @@ jQuery.fn.scale = function()
     }
     
     // Object too wide, but height is fine. Need to diet.
-    else if( oWidth > pWidth && oHeight < pHeight ){
+    else if( this.outerWidth() > this.parent().innerWidth() && 
+             this.outerHeight() < this.parent().innerHeight() ){
         
         console.log( "Object too wide, but height is fine. Need to diet." );
         
@@ -43,30 +39,35 @@ jQuery.fn.scale = function()
     
     // Object too short and skinny. Need to match the dimenstion that is
     // closer to being correct.
-    else if( oWidth < pWidth && oHeight < pHeight ){
+    else if( this.outerWidth() < this.parent().innerWidth() && 
+             this.outerHeight() < this.parent().innerHeight() ){
     
         console.log( "Object too short and skinny." );
                
-        if( Math.abs(pHeight - oHeight) < Math.abs(pWidth - oWidth) ){
+        if( Math.abs(this.parent().innerHeight() - this.outerHeight()) <= 
+            Math.abs(this.parent().innerWidth() - this.outerWidth()) ){
             
-            console.log( "Height is closer to being correct." );
+            console.log( "Height is closer to being correct OR height and " +
+                         "width are equally distant." );
             
-            matchWidth( this );
+            matchHeight( this );
             
         } else {
         
             console.log( "Width is closer to being correct." );
             
-            matchHeight( this );
+            matchWidth( this );
         }
     
     // Object too tall and wide. Need to match the dimenstion that is
     // further from being correct.
-    } else if( oWidth > pWidth && oHeight > pHeight ){
+    } else if( this.outerWidth() > this.parent().innerWidth() && 
+               this.outerHeight() > this.parent().innerHeight() ){
     
         console.log( "Object too tall and wide." );
                
-        if( Math.abs(pHeight - oHeight) > Math.abs(pWidth - oWidth) ){
+        if( Math.abs(this.parent().innerHeight() - this.outerHeight()) > 
+            Math.abs(this.parent().innerWidth() - this.outerWidth()) ){
             
             console.log( "Height is further from being correct." );
             
@@ -74,28 +75,40 @@ jQuery.fn.scale = function()
             
         } else {
         
-            console.log( "Width is further from being correct." );
+            console.log( "Width is further from being correct OR height and " +
+                         "width are equally distant." );
             
             matchWidth( this );
         }
     } else 
         console.log( "Object is the exact same size as parent. Do nothing." );
+
+    // if the center option is enabled, also center the object within the parent
+    if( center == "center" ){
+        this.css( 'position', 'relative' );
+        this.css( 'margin-top', this.parent().innerHeight()/2 - 
+                  this.outerHeight()/2 );
+        this.css( 'margin-left', this.parent().innerWidth()/2 - 
+                  this.outerWidth()/2 );
+    }
     
     // match the height while maintaining the aspect ratio
     function matchHeight( obj ){
         console.log( "Adjusting height." );
         
-        obj.width( oWidth * pHeight/oHeight - (oWidth - obj.width()) );
+        obj.width( obj.outerWidth() * obj.parent().innerHeight()/
+                   obj.outerHeight() - (obj.outerWidth() - obj.width()) );
     }
      
     // match the width while maintaining the aspect ratio
     function matchWidth( obj ){
         console.log( "Adjusting width." );
 
-        obj.height( oHeight * pWidth/oWidth - (oHeight - obj.height()) );
+        obj.height( obj.outerHeight() * obj.parent().innerWidth()/
+                    obj.outerWidth() - (obj.outerHeight() - obj.height()) );
     }
 
-    console.log("... Done safely resizing stuff.");
+    console.log("... Done proportionally scaling stuff.");
 
     // return this object for chaining    
     return this;
