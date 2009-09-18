26a6c65eaebd30b506a5da2cf151b2f02cc6f7e7
Rob McGuire-Dale <rob@osuosl.org>
2009-09-18 14:51:33 -0700

    Now works with both images and other objects (hopefully) with Firefox, Chromium, and Safari. Had to rewrite how it waits for the images to load.

--- jquery.scale.js
+++ jquery.scale.js
@@ -18,30 +18,43 @@
  * Kevin Liew
  */
 
-(function($){                           // anonymous function wrapper
+(function($){                               // anonymous function wrapper
 
-    $.fn.extend({                       // attach new method to jQuery
+    $.fn.extend({                           // attach new method to jQuery
     
         scale: function( arg1, arg2 ){      // declare plugin name and parameter
         
             // iterate over current set of matched elements
             return this.each( function() {
             
-                // force element reload
-                var date = new Date();
-                var cursrc = $(this).attr("src");
-                var newsrc = cursrc;
-                if( cursrc.indexOf('?') != -1 )
-                    newsrc = cursrc.substring( 0, cursrc.indexOf('?'));
-                newsrc = newsrc + "?" + date.getTime();
-                $(this).attr( "src", newsrc );
+                console.log( "jquery.scale is starting...");
             
-                // wait for the object to be fully loaded before doing anything
-                this.onload = function(){
-                
-                    // capture the object
-                    var obj = $(this);
+                // capture the object
+                var obj = $(this);
+            
+                // get the object back to its original size
+                obj.removeAttr( 'height' ); obj.removeAttr( 'width' );
+            
+                // force element reload if it's an image
+                if( obj.attr('src') != null ){
+                    console.log( "jquery.scale: object is an image" );
+                    var date = new Date();
+                    var cursrc = obj.attr("src");
+                    var newsrc = cursrc;
+                    if( cursrc.indexOf('?') != -1 )
+                        newsrc = cursrc.substring( 0, cursrc.indexOf('?'));
+                    newsrc = newsrc + "?" + date.getTime();
+                    obj.attr( "src", newsrc );
                     
+                    this.onload = start;
+                     
+                } else {
+                    console.log("jquery.scale: object is NOT an image");
+                    start();
+                }
+            
+                // the main plugin function
+                function start(){
                     console.log( "jquery.scale: BEFORE scaling, object's size" +
                         " = " + obj.outerWidth() + "x" + obj.outerHeight() + 
                         ", object's parent's size = " + 
